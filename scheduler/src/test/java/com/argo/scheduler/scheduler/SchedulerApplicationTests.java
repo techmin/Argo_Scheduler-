8a681cd1f0e794108033e498df5f47d4a9b39bb4
Yasmine Aitouny <aitouny680@gmail.com>
2023-10-06 12:12:32 -0500

    added dependencies

---
+++ scheduler/src/test/java/com/argo/scheduler/scheduler/SchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argo.scheduler.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
