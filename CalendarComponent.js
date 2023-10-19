289f9354c96822993a7986e19ae4467ee727d0fc
jhdz99 <125790653+jhdz99@users.noreply.github.com>
2023-10-18 19:00:49 -0500

    Update CalendarComponent.js

--- CalendarComponent.js
+++ CalendarComponent.js
@@ -1,88 +1,57 @@
 import React, { Component } from 'react';
-import { Calendar, luxonLocalizer } from 'react-big-calendar';
-import { DateTime} from 'luxon';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import interactionPlugin from '@fullcalendar/interaction'; 
 import axios from 'axios';
 import './styles.css';
 
-
-const localizer = luxonLocalizer(DateTime);
-
-// main class that extends Component to fetch html
 class CalendarComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = {      // initialize component state
-      events: [],  // fetched events from backend stored here
+    this.state = {
+      events: [],
     };
   }
 
-
-  // fetch events when components mount
   componentDidMount() {
     axios.get('backendurl/events')
     .then(response => {
-      this.setState({ events: response.data });   // updates state with fetched event
+      this.setState({ events: response.data });
     })
     .catch(error => {
-      console.error("Error fetching events:", error);   // log errors
+      console.error("Error fetching events:", error);
     });
   }
 
-
-  // Method that handles form submission for new events
-  handleSubmit = (event) => {
-    event.preventDefault();
-
-    // new event object
+  handleDateClick = (arg) => {
     const newEvent = {
-      title: this.state.title, // assuming you've bound an input field to this state variable
-      start: new Date(this.state.startDate), // assuming you've bound a date input field
-      end: new Date(this.state.endDate), // same
-      // other fields
+      title: "New Event",
+      start: arg.date,
+      allDay: arg.allDay
     };
-
-    // POST request made to backend to save new event 
-    axios.post('backendurl/events', newEvent)
-      .then(response => {
-        this.setState(prevState => ({
-          events: [...prevState.events, response.data]     // handle successful ceation by adding the new event to your state
-
-        }));
-      })
-      .catch(error => {
-        console.error("Error creating event:", error);  // log errors
-      });
+    this.setState({ events: [...this.state.events, newEvent] });
   }
 
-
-  // Component display
   render() {
     return (
       <div>
-        {/* Add Event Form */}
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" value={this.state.title} onChange={e => this.setState({ title: e.target.value })} placeholder="Event Title" />
-          <input type="date" value={this.state.startDate} onChange={e => this.setState({ startDate: e.target.value })} />
-          <input type="date" value={this.state.endDate} onChange={e => this.setState({ endDate: e.target.value })} />
-          <button type="submit">Add Event</button>
-        </form>
-
-
-        {/* Calendar Display */}
-        <Calendar
-            localizer={localizer}
-            events={this.state.events}
-            startAccessor="start"
-            endAccessor="end"
-            views={['month', 'week', 'day', 'agenda']}
-            defaultView='month'
-            style={{ height: "600px" }}  // adjust this to ensure month grid is visible
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin]}
+          initialView="dayGridMonth"
+          events={this.state.events}
+          selectable={true}
+          dateClick={this.handleDateClick}
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+          }}
         />
       </div>
     );
   }
 }
 
-// exports for use in other parts of app
 export default CalendarComponent;
