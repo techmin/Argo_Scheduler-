9a51f3abf8666effe1a143bb1a9d6a9cfe7bd3c2
Yasmine Aitouny <aitouny680@gmail.com>
2023-10-26 21:02:50 -0500

    add cron expression class

--- scheduler/src/main/java/com/example/scheduler/jobs/JobExecutor.java
+++ scheduler/src/main/java/com/example/scheduler/jobs/JobExecutor.java
@@ -1,30 +1,54 @@
 package com.example.scheduler.jobs;
 
 import org.quartz.Job;
-import org.quartz.JobDataMap;
+// import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import java.util.*;
 
-import com.example.scheduler.info.TimerInfo;
+import com.example.scheduler.info.Task;
+import com.example.scheduler.Demo.TaskService;
 
 
 @Component
 public class JobExecutor implements Job{
     private static final Logger LOG = LoggerFactory.getLogger(JobExecutor.class);
+
+    @Autowired
+    private TaskService taskService;
     
     @Override
     // this method will execute when the job is triggered
     public void execute(JobExecutionContext context){
-        // JobDataMap retrieved from the "JobExecutionContext"
-        JobDataMap jobDataMap = context.getJobDetail().getJobDataMap();
 
-        // An instance of TimerInfo, 
-        // it fetches an object from JobDataMap using getsimplename as a key
-        TimerInfo info = (TimerInfo) jobDataMap.get(JobExecutor.class.getSimpleName());
+        //list of tasks to fetch data from database
+        List<Task> tasksToExecute = taskService.fetchTasksFromDatabase();
 
-        // to check if it runs, suppose to retrieve data stored in its job data
-        LOG.info(info.getCallbackData());
+        //execute each task
+        for(Task task: tasksToExecute){
+            taskService.executeTask(task);
+            taskService.updateTaskInDatabase(task);
+        }
+
+        
+
+
+
+
+
+
+
+        // // JobDataMap retrieved from the "JobExecutionContext"
+        // JobDataMap jobDataMap = context.getJobDetail().getJobDataMap();
+
+        // // An instance of TimerInfo, 
+        // // it fetches an object from JobDataMap using getsimplename as a key
+        // TimerInfo info = (TimerInfo) jobDataMap.get(JobExecutor.class.getSimpleName());
+
+        // // to check if it runs, suppose to retrieve data stored in its job data
+        // LOG.info(info.getCallbackData());
     }
 }
