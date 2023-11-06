92ebaba98a42838945c7002b4cf0a378504c4f74
Yasmine Aitouny <aitouny680@gmail.com>
2023-11-05 21:42:19 -0600

    added recurrence and appointments entities and repos

--- scheduler/src/main/java/com/example/scheduler/timerService/SchedulerService.java
+++ scheduler/src/main/java/com/example/scheduler/timerService/SchedulerService.java
@@ -3,8 +3,12 @@ package com.example.scheduler.timerService;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import com.example.scheduler.info.TimerInfo;
+import com.example.scheduler.repositories.AppointmentsRep;
 import com.example.scheduler.repositories.JobPropertyRep;
 import com.example.scheduler.entities.JobProperty;
+import com.example.scheduler.entities.Appointments;
+
+import java.time.LocalDate;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,11 +29,13 @@ public class SchedulerService {
     private static final Logger LOG = LoggerFactory.getLogger(SchedulerService.class);
     private final Scheduler scheduler;
     private JobPropertyRep jobRepository;
+    private AppointmentsRep appRepository;
 
     @Autowired
-    public SchedulerService(Scheduler scheduler, JobPropertyRep jobRepository){
+    public SchedulerService(Scheduler scheduler, JobPropertyRep jobRepository, AppointmentsRep appRepository){
         this.scheduler = scheduler;
         this.jobRepository = jobRepository;
+        this.appRepository = appRepository;
     }
     
 
@@ -66,15 +72,28 @@ public class SchedulerService {
         return jobRepository.save(existingJob);
     }
 
+    public Iterable<Appointments> createAppointments(LocalDate startOn){
 
-    public SchedulerService(Scheduler scheduler){
-        this.scheduler = scheduler;
     }
 
-    
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+    // public SchedulerService(Scheduler scheduler){
+    //     this.scheduler = scheduler;
+    // }
 
     // public void schedule(final Class <? extends Job> jobClass, final TimerInfo info){
     //     // utilizing functions from the TaskBuilder class
