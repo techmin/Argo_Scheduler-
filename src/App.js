43edae3d45b5a1dcb293ff6d2866d3774d9fd4bb
jhdz99 <125790653+jhdz99@users.noreply.github.com>
2023-11-21 14:03:16 -0600

    add calender

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import CalendarComponent from './CalendarComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ARGO</h1>
+      <CalendarComponent />
+    </div>
+  );
+}
+
+export default App;
